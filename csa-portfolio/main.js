a2d902ade1bb2c05c028d6aa0a40a3b4c75a5176
Carolina Stierli de Abreu <stiec708@newschool.edu>
2019-02-07 20:03:33 -0500

    relocate

---
+++ csa-portfolio/main.js
@@ -0,0 +1,25 @@
+var slideIndex = [1,1,1,1,1,1,1,1];
+var slideId = ["poem", "book", "poster", "type", "osfa", "hell", "people","bagel"]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+showDivs(1, 4);
+showDivs(1, 5);
+showDivs(1, 6);
+showDivs(1, 7);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
